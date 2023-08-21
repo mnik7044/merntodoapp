adc31f8759f05b29f6dd5683d0eecef6fb8f816a
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-08-22 05:15:42 +0530

    Done with delete

--- client/src/App.js
+++ client/src/App.js
@@ -34,6 +34,13 @@ const completeTodo = async id =>{
   }))
 }
 
+const deleteTodo = async id => {
+  const data = await fetch(API_BASE + "/todos/delete/" + id, {method: 'DELETE'} )
+  .then(res =>res.json())
+
+  setTodos(todos => todos.filter(todo => todo._id != data._id))
+}
+
   return (
     <div className="App">
      <h1> Welcome User</h1>
@@ -46,7 +53,7 @@ const completeTodo = async id =>{
        }  key={todo._id} onClick={() => completeTodo(todo._id)}>
         <div className = "checkbox"></div>
         <div className="text"> {todo.text}</div>
-        <div className ="delete-todo">x</div>
+        <div className ="delete-todo" onClick={() => deleteTodo (todo._id)}>x</div>
       </div>
       ))}
      
