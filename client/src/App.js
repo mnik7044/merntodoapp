be6216e53f1115d6e5ef091f43982f75e14f2093
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-08-22 05:03:35 +0530

    Complete DOne

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,20 @@ function App() {
     .then(data => setTodos(data))
     .catch(err => console.error("Error: ", err))
   }
+//{"todo" + (todo.complete ? "is-complete" : "" )}
 
+const completeTodo = async id =>{
+  const data = await fetch(API_BASE + "/todos/complete/" + id, {method: 'PUT'} )
+  .then(res =>res.json())
+
+  setTodos(todos => todos.map(todo => {
+    if(todo._id === data._id){
+      todo.complete = data.complete
+    }
+
+    return todo
+  }))
+}
 
   return (
     <div className="App">
@@ -28,7 +41,9 @@ function App() {
      <div className ="todos"> 
      
 
-    {todos.map(todo => ( <div className="todo" key={todo._id}>
+    {todos.map(todo =>
+       ( <div className={"todo " + (todo.complete ? "is-complete" : "" )
+       }  key={todo._id} onClick={() => completeTodo(todo._id)}>
         <div className = "checkbox"></div>
         <div className="text"> {todo.text}</div>
         <div className ="delete-todo">x</div>
