4153370c5feb2c0500ef3dfdb7287eb689065de1
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-08-22 06:28:15 +0530

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,22 @@ const deleteTodo = async id => {
   setTodos(todos => todos.filter(todo => todo._id != data._id))
 }
 
+const addTodo = async() => {
+  const data = await fetch(API_BASE + "/todos/new" , {
+    method : "POST" ,
+    headers:{
+       "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: newTodo
+      }) 
+}).then(res => res.json())
+console.log(data)
+
+setTodos([...todos, data])
+setPopupActive(false)
+setNewTodo("")
+}
   return (
     <div className="App">
      <h1> Welcome User</h1>
@@ -61,6 +77,22 @@ const deleteTodo = async id => {
    
       
      </div>
+     <div className= "addPopup" onClick ={()=> setPopupActive(true)}>+</div>
+
+     {popupActive? ( 
+      <div className = "popup">
+      <div className = "closePopup" onClick={() => setPopupActive
+        (false)}> X </div>
+      <div className="content">
+        <h3> Add Task </h3> 
+        <input type="text" className="add-todo-input" onChange={e=> setNewTodo(e.target.value) }
+        value={newTodo}/>
+        <div className= "button" onClick={addTodo}> Create Task </div>
+
+        </div>
+        </div>
+      
+     ): ''}
     </div>
     
   );
